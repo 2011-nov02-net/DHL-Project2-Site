a05bca82e5c4b376270c8163f0a80b6ecca72896
harelfishbein <harelfishbein@github.com>
2020-12-21 10:22:36 -0800

    add routing

--- DHL-Project2-Site/src/app/app.module.ts
+++ DHL-Project2-Site/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
