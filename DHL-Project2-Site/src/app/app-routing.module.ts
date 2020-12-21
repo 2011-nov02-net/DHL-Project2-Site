fe53c2150fe9d333f724fc96051a1b0864d06da4
David Barnes <david.barnes0402@gmail.com>
2020-12-21 15:59:00 -0500

    User email is now stored in session and shows up on the home page.

--- DHL-Project2-Site/src/app/app-routing.module.ts
+++ DHL-Project2-Site/src/app/app-routing.module.ts
@@ -5,8 +5,9 @@ import { RouterModule, Routes } from '@angular/router'
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+
 const routes: Routes = [
-  { path: 'app', component: AppComponent },
+  { path: '', component: AppComponent },
   { path: 'login', component: LoginComponent }
 ];
 
--- DHL-Project2-Site/src/app/login/login.component.ts
+++ DHL-Project2-Site/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { User } from '../shared/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +10,21 @@ import { User } from '../shared/user.model';
 })
 export class LoginComponent implements OnInit {
 
+  myForm: FormGroup;
+  // email = new FormControl('');
   user:User
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.myForm = new FormGroup({'email': new FormControl('')})
   }
 
-  loginUser(event) {
-    console.log(event);
+  loginUser() {
+    console.log(this.myForm.get('email').value);
+    sessionStorage.setItem('currentEmail', this.myForm.get('email').value );
+    this.router.navigate(['/']);
   }
 
 }
