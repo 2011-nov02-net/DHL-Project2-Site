4285940b4873d32941d01b4e65871baeff5b6840
harelfishbein <harelfishbein@github.com>
2020-12-22 14:59:42 -0800

    removed extra buttons from home

--- DHL-Project2-Site/src/welcome/welcome.component.ts
+++ DHL-Project2-Site/src/welcome/welcome.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../shared/Models/user.model';
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-
+  user : User;
   constructor() { }
 
   ngOnInit(): void {
