f16973899ad0749b1458251f207a289a5d8aefe8
Luke Fisher <thebigfishluke@gmail.com>
2020-12-23 17:37:00 -0500

    user sign up functionality is working

--- DHL-Project2-Site/src/shared/Services/user.service.ts
+++ DHL-Project2-Site/src/shared/Services/user.service.ts
@@ -12,14 +12,20 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
+  user: User;
   private baseUrl = `${environment.baseUrl}/api`;
 
   httpOptions = {
     headers: new HttpHeaders({'Content-Type': "application/json"})
   };
 
-  signUpUser(user: User): Promise<User> {
-    return this.http.post<User>(`${this.baseUrl}/User`, user, this.httpOptions)
+  signUpUser(user:User): Promise<User> {
+    return this.http.post<User>(`${this.baseUrl}/User?name=${user.fullName}&email=${user.email}&permission=${user.permission}`, this.httpOptions)
+    .toPromise();
+  }
+
+  updateUser(user:User): Promise<User> {
+    return this.http.put<User>(`${this.baseUrl}/User/${user.id}`, {}, this.httpOptions)
     .toPromise();
   }
   
--- DHL-Project2-Site/src/user/user.component.ts
+++ DHL-Project2-Site/src/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/shared/Models/user.model';
 import { UserService } from 'src/shared/Services/user.service';
@@ -10,20 +10,43 @@ import { UserService } from 'src/shared/Services/user.service';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  myForm: FormGroup;
 
-  constructor(private router: Router, private userService: UserService) { }
+  constructor(private router: Router, 
+              private userService: UserService,
+              private fb: FormBuilder
+              ) { }
+
+
+    myForm: FormGroup;
+    fullName: FormControl;
+    email: FormControl;
+    permission: FormControl;
 
   ngOnInit(): void {
-    this.myForm = new FormGroup({'fullName': new FormControl(''), 'email': new FormControl(''), 'permission': new FormControl(2)});
+    this.fullName = new FormControl('');
+    this.email = new FormControl('');
+    this.permission = new FormControl('');
+
+    this.myForm = this.fb.group(
+      {
+        'fullName': this.fullName,
+        'email': this.email,
+        'permission': this.permission
+      }
+
+    )
   }
+
+    
 
   async signUp() {
     debugger;
     console.log(this.myForm.get('fullName').value);
     console.log(this.myForm.get('email').value);
+    var fullName = this.myForm.get('fullName').value;
+    var email = this.myForm.get('email').value;
     sessionStorage.setItem('currentEmail', this.myForm.get('email').value);
-    let newUser = new User(this.myForm.get('fullName').value, this.myForm.get('email').value);
+    var newUser = new User(fullName, email);
     await this.userService.signUpUser(newUser);
     this.router.navigate(['/course']);
   }
