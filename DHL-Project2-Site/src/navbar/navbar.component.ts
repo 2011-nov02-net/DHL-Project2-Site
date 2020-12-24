73cb557d6f3252092b964b3f0762abf7325c22c5
harelfishbein <harelfishbein@github.com>
2020-12-24 03:10:08 -0800

    moved logout function to navbar

--- DHL-Project2-Site/src/navbar/navbar.component.ts
+++ DHL-Project2-Site/src/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { LoginService } from '../shared/Services/login.service';
 import { User } from '../shared/Models/user.model';
 
 @Component({
@@ -8,11 +10,22 @@ import { User } from '../shared/Models/user.model';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  loggedIn: boolean;
   user: User;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private data: LoginService
+  ) { }
 
   ngOnInit(): void {
+    this.data.currentUser.subscribe(user => this.user = user)
+  }
+
+  logout(): void {
+    this.data.setUser(null);
+    sessionStorage.removeItem('currentEmail');
+    this.router.navigate(['/']);
   }
 
 }
